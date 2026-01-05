405a39675bc5b1ff2468806f553a01beadc6e58c
jasenmp <gtm4y5kghx@privaterelay.appleid.com>
2026-01-05 06:31:10 -0600

    Wiring up the sales asset post type.

--- includes/class-post-type.php
+++ includes/class-post-type.php
@@ -10,7 +10,66 @@ if (!defined('ABSPATH')) {
 class RS_Sales_Post_Type {
     
     public static function register() {
-        // TODO: Register rs_sales_asset post type
-        // TODO: Register asset_category taxonomy
+        self::register_post_type();
+        self::register_taxonomy();
+    }
+
+     private static function register_post_type() {
+        $labels = [
+            'name'               => 'Sales Assets',
+            'singular_name'      => 'Sales Asset',
+            'add_new'            => 'Add New',
+            'add_new_item'       => 'Add New Asset',
+            'edit_item'          => 'Edit Asset',
+            'new_item'           => 'New Asset',
+            'view_item'          => 'View Asset',
+            'search_items'       => 'Search Assets',
+            'not_found'          => 'No assets found',
+            'not_found_in_trash' => 'No assets found in trash',
+            'menu_name'          => 'Sales Assets',
+        ];
+
+        $args = [
+            'labels'             => $labels,
+            'public'             => false,
+            'publicly_queryable' => false,
+            'show_ui'            => true,
+            'show_in_menu'       => true,
+            'query_var'          => false,
+            'capability_type'    => 'post',
+            'has_archive'        => false,
+            'hierarchical'       => false,
+            'menu_position'      => 20,
+            'menu_icon'          => 'dashicons-media-document',
+            'supports'           => ['title', 'editor', 'thumbnail'],
+            'show_in_rest'       => false,
+        ];
+
+        register_post_type('rs_sales_asset', $args);
+    }
+
+    private static function register_taxonomy() {
+        $labels = [
+            'name'              => 'Asset Categories',
+            'singular_name'     => 'Asset Category',
+            'search_items'      => 'Search Categories',
+            'all_items'         => 'All Categories',
+            'edit_item'         => 'Edit Category',
+            'update_item'       => 'Update Category',
+            'add_new_item'      => 'Add New Category',
+            'new_item_name'     => 'New Category Name',
+            'menu_name'         => 'Categories',
+        ];
+
+        $args = [
+            'labels'            => $labels,
+            'hierarchical'      => true,
+            'public'            => false,
+            'show_ui'           => true,
+            'show_admin_column' => true,
+            'show_in_rest'      => false,
+        ];
+
+        register_taxonomy('rs_asset_category', 'rs_sales_asset', $args);
     }
 }
